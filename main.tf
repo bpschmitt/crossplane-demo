84bfbf67b0957c5a799c2527534194f8c48664b9
bschmitt <bschmitt@newrelic.com>
2023-09-06 11:40:46 -0500

    update provider source

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "newrelic" {
+    account_id = var.nr_account_id
+    api_key = var.nr_api_key
+    region = var.nr_region
+}
+
 module "crossplane-newrelic" {
   source  = "./module"
 }
--- module/main.tf
+++ module/main.tf
@@ -1,10 +1,3 @@
-provider "newrelic" {
-    source  = "newrelic/newrelic"
-    account_id = var.nr_account_id
-    api_key = var.nr_api_key
-    region = var.nr_region
-}
-
 resource "newrelic_alert_policy" "kubernetes_alert_policy" {
     name = var.nr_alert_policy_name
 }
